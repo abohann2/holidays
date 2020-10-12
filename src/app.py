1f68f9d369b97c0a833a466631d48670f31a3766
Amanda Bohannon <abohann2@gmail.com>
2020-10-12 16:45:43 -0500

    basic starting point for formating api response

--- src/app.py
+++ src/app.py
@@ -1,6 +1,17 @@
-from flask import Flask, render_template
+import os
+import requests
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
+API_KEY = os.environ.get("API_KEY")
+
+@app.route('/', methods=["POST", "GET"])
 def hello_world():
-    return render_template("index.html")
+
+    if request.method == "GET":
+        response = requests.get(f"https://calendarific.com/api/v2/holidays?api_key={API_KEY}&country=IN&year=2020")
+        holidays = response.json()['response']['holidays']
+
+        print(len(holidays))
+
+    return render_template("index.html", holidays=holidays)
